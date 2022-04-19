f6b066ff1dee659c4331c23937fcf1201f20ebdc
frostzzone <65735427+frostzzone@users.noreply.github.com>
2022-04-19 15:32:16 +0000

    Pikachu meme

---
+++ pages/api/pikachu.js
@@ -0,0 +1,28 @@
+var Jimp = require('jimp');
+
+function isValidUrl(string) {
+    try {
+      new URL(string);
+    } catch (_) {
+      return false;  
+    }
+
+    return true;
+}
+
+export default async (req, res) => {
+  let text = req.query.text
+  if((text==null) || (text==undefined)) return res.status(200).setHeader('Content-Type', 'application/json').send(JSON.stringify({error: "provide some text", example: "/pikachu?text=macncheese"}, null, 4));
+  
+  Jimp.read('https://poopoo-api.vercel.app/images/pikachu.png', (err, pika) => {
+    Jimp.loadFont(Jimp.FONT_SANS_128_BLACK).then(font => {
+      pika.print(font, 20, 10, {
+                                text: text,
+                            }, pika.bitmap.width, pika.bitmap.height);
+  pika.getBuffer(Jimp.MIME_PNG, function(err, buffer){
+             res.setHeader("Content-Type", 'image/png');
+             res.send(buffer);
+         });
+    })
+  })
+}
