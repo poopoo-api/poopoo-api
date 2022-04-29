e2a5e0d158f3450f6b9e08cd55569280b5d341d1
frostzzone <65735427+frostzzone@users.noreply.github.com>
2022-04-29 17:46:51 +0000

    pls work

--- pages/api/image/bobross.js
+++ pages/api/image/bobross.js
@@ -29,5 +29,4 @@ Jimp.read(url, (err, art) => {
 })
 })
 })
-
 }
--- pages/api/image/gen.js
+++ pages/api/image/gen.js
@@ -4,7 +4,7 @@ export default async (req, res) => {
   info['/image/gen'] = ["This page","Gives info about the other pages here","No actual use except to give info"]
   info['/image/ad'] = ["Ooooooo an ad","/ad?url=https://poopoo-api.vercel.app/images/example.png"]
   info['/image/beautiful'] = ["Now this... this is beautiful","/beautiful?url=https://poopoo-api.vercel.app/images/example.png"]
-  info['/image/boboross'] = ["Now this is art","/bobross?url=https://poopoo-api.vercel.app/images/example.png"]
+  info['/image/boboross'] = ["Now this is art","/bobross?url=https://poopoo-api.vercel.app/images/example.png"] 
   info['/image/qr'] = ["Generate a qrcode from an input","/qr?input=Hello"]
   
   res.setHeader("Content-Type", "application/json")
