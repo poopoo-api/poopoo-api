074b6a9813ca90eab041e257ad61726f46971d16
Eruption <94153406+EruptionGuy@users.noreply.github.com>
2022-09-08 16:00:01 +0000

    [⚠️] More changes to API page!

--- public/search.js
+++ public/search.js
@@ -1,1 +1,24 @@
-// Searching
+const articles = Array.from(document.getElementsByClassName("card"));
+const input = document.getElementById("search");
+
+const articlesArray = [];
+
+articles.map((element) => {
+	articlesArray.push({
+		title: element.dataset.tittle,
+		element: element
+	})
+})
+
+input.addEventListener("input", (e) => {
+	console.log(e.target.value)
+  const value = e.target.value ? e.target.value.toLowerCase() : ""
+  articlesArray.map((element) => {
+	  if(element.title) {
+		  const matched = element.title.toLowerCase().includes(value)
+			console.log(matched)
+	  
+    		element.element.classList.toggle("hide", !matched);
+	  }
+  });
+});
