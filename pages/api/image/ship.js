31b0920951eb0836c11954301c228c06f7b4d0f2
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-10-03 13:33:40 -0700

    added ship api
    oh and also cleaned up poopy-doopy-i-saw-a-scoopy.js

---
+++ pages/api/image/ship.js
@@ -0,0 +1,28 @@
+var Jimp = require('jimp');
+
+function isValidUrl(string) {
+    try {
+        new URL(string);
+    } catch (_) {
+        return false;  
+    }
+
+    return true;
+}
+
+export default async (req, res) => {
+    let p1 = req.query.image1
+    let p2 = req.query.image2
+    let mat = req.query.good.toLowercase() == 'true'
+    if((p1==null || p2==null) || (p1==undefined || p2==undefined)) return res.status(200).setHeader('Content-Type', 'application/json').send(JSON.stringify({error: "provide two images", example: "/ship?image1=https://poopoo-api.vercel.app/images/api/goodMatch.png&image2=https://poopoo-api.vercel.app/images/api/badMatch.png&good=false"}, null, 4));
+    if(!(isValidUrl(p1) && isValidUrl(p2))) return res.status(200).setHeader('Content-Type', 'application/json').send(JSON.stringify({error: "provide a valid url for image1 and image2", example: "/ship?image1=https://poopoo-api.vercel.app/images/api/goodMatch.png&image2=https://poopoo-api.vercel.app/images/api/badMatch.png&good=false"}, null, 4));
+    Jimp.read('https://poopoo-api.vercel.app/images/api/shipBase.png', (err, ship) => {
+        Jimp.blit(jimp.read(p1), 0, 0)
+        Jimp.blit(jimp.read(p2), 168, 0)
+        Jimp.blit(jimp.read(mat ? 'https://poopoo-api.vercel.app/images/api/goodMatch.png' : 'https://poopoo-api.vercel.app/images/api/badMatch.png'), 112, 32)
+        ship.getBuffer(Jimp.MIME_PNG, function(err, buffer){
+            res.setHeader("Content-Type", 'image/png');
+            res.send(buffer);
+        });
+    })
+}
--- pages/api/poopy-doopy-i-saw-a-scoopy.js
+++ pages/api/poopy-doopy-i-saw-a-scoopy.js
@@ -1,12 +1,12 @@
-  import fetch from 'node-fetch';
+import fetch from 'node-fetch';
+
 export default async(req, res) => {
-    const object = req.body
-  
-    if(object["poopycode"] == process.env['poopydoopy']) { 
-const response = await fetch(object["url"]);
-const data = await response.json();
-  res.send(JSON.stringify(data, null, 4))
-                                                         } else {
+  const object = req.body
+  if(object["poopycode"] == process.env['poopydoopy']) { 
+    const response = await fetch(object["url"]);
+    const data = await response.json();
+    res.send(JSON.stringify(data, null, 4))
+  } else {
     res.send(JSON.stringify({"response": "No poopy?"}, null, 4))
-    }
+  }
 }
