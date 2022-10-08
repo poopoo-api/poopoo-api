2c40186270a225feb56253021b03eb0541e05c78
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-10-07 21:42:14 -0700

    commit

--- pages/api/image/ship.js
+++ pages/api/image/ship.js
@@ -14,22 +14,26 @@ function isValidUrl(string) {
 }
 
 export default async (req, res) => {
-    const p1 = req.query.image1
-    const p2 = req.query.image2
-    const mat = req.query.good == 'true' || req.query.good == 'TRUE'
-    if((p1==null || p2==null) || (p1==undefined || p2==undefined)) return res.status(200).setHeader('Content-Type', 'application/json').send(JSON.stringify({error: "provide two images", example: "/ship?image1=https://poopoo-api.vercel.app/images/api/goodMatch.png&image2=https://poopoo-api.vercel.app/images/api/badMatch.png&good=false"}, null, 4));
-    if(!(isValidUrl(p1) && isValidUrl(p2))) return res.status(200).setHeader('Content-Type', 'application/json').send(JSON.stringify({error: "provide a valid url for image1 and image2", example: "/ship?image1=https://poopoo-api.vercel.app/images/api/goodMatch.png&image2=https://poopoo-api.vercel.app/images/api/badMatch.png&good=false"}, null, 4));
-    Jimp.read('https://poopoo-api.vercel.app/images/api/shipBase.png', async (err, ship) => {
-        if (err) {
-            res.send(JSON.stringify({"error": err}))
-            return
-        }
-        Jimp.blit(await jimp.read(p1), 0, 0)
-        Jimp.blit(await jimp.read(p2), 168, 0)
-        Jimp.blit(mat ? good : bad, 112, 32)
-        ship.getBuffer(Jimp.MIME_PNG, function(err, buffer){
-            res.setHeader("Content-Type", 'image/png');
-            res.send(buffer);
-        });
-    })
+    try {
+        const p1 = req.query.image1
+        const p2 = req.query.image2
+        const mat = req.query.good == 'true' || req.query.good == 'TRUE'
+        if((p1==null || p2==null) || (p1==undefined || p2==undefined)) return res.status(200).setHeader('Content-Type', 'application/json').send(JSON.stringify({error: "provide two images", example: "/ship?image1=https://poopoo-api.vercel.app/images/api/goodMatch.png&image2=https://poopoo-api.vercel.app/images/api/badMatch.png&good=false"}, null, 4));
+        if(!(isValidUrl(p1) && isValidUrl(p2))) return res.status(200).setHeader('Content-Type', 'application/json').send(JSON.stringify({error: "provide a valid url for image1 and image2", example: "/ship?image1=https://poopoo-api.vercel.app/images/api/goodMatch.png&image2=https://poopoo-api.vercel.app/images/api/badMatch.png&good=false"}, null, 4));
+        Jimp.read('https://poopoo-api.vercel.app/images/api/shipBase.png', async (err, ship) => {
+            if (err) {
+                res.send(JSON.stringify({"error": err}))
+                return
+            }
+            Jimp.blit(await jimp.read(p1), 0, 0)
+            Jimp.blit(await jimp.read(p2), 168, 0)
+            Jimp.blit(mat ? good : bad, 112, 32)
+            ship.getBuffer(Jimp.MIME_PNG, function(err, buffer){
+                res.setHeader("Content-Type", 'image/png');
+                res.send(buffer);
+            });
+        })
+    } catch (err) {
+        res.send(JSON.stringify({"error": err}))
+    }
 }
