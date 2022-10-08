bd647bd5134c382d03ae545e057e474e0a8e8d59
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-10-07 21:39:11 -0700

    cummon

--- pages/api/image/ship.js
+++ pages/api/image/ship.js
@@ -1,5 +1,8 @@
 var Jimp = require('jimp');
 
+const good = jimp.read('https://poopoo-api.vercel.app/images/api/goodMatch.png') // pre define these so that they dont take upspace on the blit
+const bad = jimp.read('https://poopoo-api.vercel.app/images/api/badMatch.png') // and also should hopfully speed it up so it doesnt time out so much
+
 function isValidUrl(string) {
     try {
         new URL(string);
@@ -11,15 +14,19 @@ function isValidUrl(string) {
 }
 
 export default async (req, res) => {
-    let p1 = req.query.image1
-    let p2 = req.query.image2
-    let mat = req.query.good == 'true' || req.query.good == 'TRUE'
+    const p1 = req.query.image1
+    const p2 = req.query.image2
+    const mat = req.query.good == 'true' || req.query.good == 'TRUE'
     if((p1==null || p2==null) || (p1==undefined || p2==undefined)) return res.status(200).setHeader('Content-Type', 'application/json').send(JSON.stringify({error: "provide two images", example: "/ship?image1=https://poopoo-api.vercel.app/images/api/goodMatch.png&image2=https://poopoo-api.vercel.app/images/api/badMatch.png&good=false"}, null, 4));
     if(!(isValidUrl(p1) && isValidUrl(p2))) return res.status(200).setHeader('Content-Type', 'application/json').send(JSON.stringify({error: "provide a valid url for image1 and image2", example: "/ship?image1=https://poopoo-api.vercel.app/images/api/goodMatch.png&image2=https://poopoo-api.vercel.app/images/api/badMatch.png&good=false"}, null, 4));
-    Jimp.read('https://poopoo-api.vercel.app/images/api/shipBase.png', (err, ship) => {
-        Jimp.blit(jimp.read(p1), 0, 0)
-        Jimp.blit(jimp.read(p2), 168, 0)
-        Jimp.blit(jimp.read(mat ? 'https://poopoo-api.vercel.app/images/api/goodMatch.png' : 'https://poopoo-api.vercel.app/images/api/badMatch.png'), 112, 32)
+    Jimp.read('https://poopoo-api.vercel.app/images/api/shipBase.png', async (err, ship) => {
+        if (err) {
+            res.send(JSON.stringify({"error": err}))
+            return
+        }
+        Jimp.blit(await jimp.read(p1), 0, 0)
+        Jimp.blit(await jimp.read(p2), 168, 0)
+        Jimp.blit(mat ? good : bad, 112, 32)
         ship.getBuffer(Jimp.MIME_PNG, function(err, buffer){
             res.setHeader("Content-Type", 'image/png');
             res.send(buffer);
