19cb06de54b846477da3f93b974ce77e415ad422
frostzzone <65735427+frostzzone@users.noreply.github.com>
2022-10-21 18:34:46 +0000

    nanoid size customization

--- pages/api/nanoid.js
+++ pages/api/nanoid.js
@@ -1,8 +1,9 @@
 import { nanoid } from 'nanoid'
 
 export default (req, res) => {
+    let length = req.query.length || 10
     let object = new Object();
-    object['nanoid'] = nanoid();
+    object['nanoid'] = nanoid(length);
     res.setHeader("Content-Type", 'application/json');
     res.send(JSON.stringify(object, null, 4));
 }
