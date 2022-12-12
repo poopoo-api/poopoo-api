913dd7ed3ef7bb7e9e3af33c2bee9d1660c4e81e
frostzzone <65735427+frostzzone@users.noreply.github.com>
2022-12-12 08:40:02 -0800

    Update poopy-doopy-i-saw-a-scoopy.js

--- pages/api/poopy-doopy-i-saw-a-scoopy.js
+++ pages/api/poopy-doopy-i-saw-a-scoopy.js
@@ -1,12 +1,13 @@
-  import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 export default async(req, res) => {
-    const object = req.body
+  const object = req.body
   
-    if(object["poopycode"] == process.env['poopydoopy']) { 
-const response = await fetch(object["url"]);
-const data = await response.json();
-  res.send(JSON.stringify(data, null, 4))
-                                                         } else {
+  if(object["poopycode"] == process.env['poopydoopy']) { 
+    let options = object["options"] || {};
+    const response = await fetch(object["url"], options);
+    const data = await response.json();
+     res.send(JSON.stringify(data, null, 4))
+  } else {
     res.send(JSON.stringify({"response": "No poopy?"}, null, 4))
-    }
+  }
 }
